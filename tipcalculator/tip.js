34b76d4f2cd6eafc456ac370cea3f07ca80c498c
Han Truong <hantruongth@gmail.com>
2019-06-30 22:45:14 -0500

    add tip calculator

---
+++ tipcalculator/tip.js
@@ -0,0 +1,13 @@
+
+function calcTip() {
+	var subtotalElem = document.getElementById('subtotal');
+	var tipElem = document.getElementById('tip');
+	var totalElem = document.getElementById('total');
+    var subtotal = parseFloat(subtotalElem.value !== "" ? subtotalElem.value : 0);
+    var tip = parseFloat(tipElem.value !== "" ? tipElem.value : 0);
+    
+
+
+	var total = subtotal + subtotal*tip/100;
+	totalElem.innerHTML = '$' + total;
+}
