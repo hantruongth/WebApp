bec4a8d117f49dd45d6b880ae6e2fafc1ce526f9
Han Truong <hantruongth@gmail.com>
2019-07-02 13:51:11 -0500

    update js using modules

--- ASCIImation/ascii.js
+++ ASCIImation/ascii.js
@@ -1,86 +1,88 @@
-"use strict";
+(function(){
+    "use strict";
 
-var timer = null;
-var animationArea;
-var animationArray = [];
-var start = 0;
-var end = 0;
-var interval = 250;
-var i;
-var animationType;
-var animationsList = ANIMATIONS; // from animations.js provided 
+    var timer = null;
+    var animationArea;
+    var animationArray = [];
+    var start = 0;
+    var end = 0;
+    var interval = 250;
+    var i;
+    var animationType;
+    var animationsList = ANIMATIONS; // from animations.js provided 
 
-window.onload = function () {
-    var btnPlay = document.getElementById('btnPlay');
-    var btnStop = document.getElementById('btnStop');
-    var turboCheckbox = document.getElementById('turboCheckbox');
-    var animationSize = document.getElementById('animationSize');
-    animationArea = document.getElementById("animationArea");
-    animationType = document.getElementById('animationType');
+    window.onload = function () {
+        var btnPlay = document.getElementById('btnPlay');
+        var btnStop = document.getElementById('btnStop');
+        var turboCheckbox = document.getElementById('turboCheckbox');
+        var animationSize = document.getElementById('animationSize');
+        animationArea = document.getElementById("animationArea");
+        animationType = document.getElementById('animationType');
 
-    animationType.onchange = function () {
-        play();
+        animationType.onchange = function () {
+            play();
+        };
+
+        btnPlay.onclick = function () {
+            play();
+        };
+
+        btnStop.onclick = function () {   
+            animationArea.innerHTML = "";
+            clearInterval(timer);
+            timer = null; 
+            setBtnState();
+            
+        };
+
+        turboCheckbox.onchange = function () {
+            if (turboCheckbox.checked) {
+                interval = 50;
+            } else {
+                interval = 250;
+            }
+            restart();
+        };
+        animationSize.onchange = function () {
+            animationArea.style.fontSize = this.options[this.selectedIndex].value + "pt";
+            restart();
+        };
     };
-
-    btnPlay.onclick = function () {
-        play();
-    };
-
-    btnStop.onclick = function () {   
-        animationArea.innerHTML = "";
-        clearInterval(timer);
-        timer = null; 
-        setBtnState();
-        
-    };
-
-    turboCheckbox.onchange = function () {
-        if (turboCheckbox.checked) {
-            interval = 50;
-        } else {
-            interval = 250;
-        }
-        restart();
-    };
-    animationSize.onchange = function () {
-        animationArea.style.fontSize = this.options[this.selectedIndex].value + "pt";
-        restart();
-    };
-};
-function play() {
-    var selectedIndex = animationType.options[animationType.selectedIndex].value;
-    if (selectedIndex < 0) return;
-    animationArray = animationsList[selectedIndex].split("=====\n");
-    i = start;
-    end = animationArray.length;
-    if (timer === null) {
-        timer = setInterval(animate, interval); // interval 250
-    }
-    setBtnState();
-
-}
-
-function animate() {
-    if (i < end) {
-        animationArea.value = animationArray[i];
-        i++;
-    }
-    else {
+    function play() {
+        var selectedIndex = animationType.options[animationType.selectedIndex].value;
+        if (selectedIndex < 0) return;
+        animationArray = animationsList[selectedIndex].split("=====\n");
         i = start;
-        animationArea.value = animationArray[i];
-    }
-}
-function restart() {
-    clearInterval(timer);
-    timer = setInterval(animate, interval);
-}
+        end = animationArray.length;
+        if (timer === null) {
+            timer = setInterval(animate, interval); // interval 250
+        }
+        setBtnState();
 
-function setBtnState(){
-    if(timer == null){
-        btnStop.disabled = true;
-        btnPlay.disabled = false;
-    }else{
-        btnPlay.disabled = true;
-        btnStop.disabled = false;
     }
-}
+
+    function animate() {
+        if (i < end) {
+            animationArea.value = animationArray[i];
+            i++;
+        }
+        else {
+            i = start;
+            animationArea.value = animationArray[i];
+        }
+    }
+    function restart() {
+        clearInterval(timer);
+        timer = setInterval(animate, interval);
+    }
+
+    function setBtnState(){
+        if(timer == null){
+            btnStop.disabled = true;
+            btnPlay.disabled = false;
+        }else{
+            btnPlay.disabled = true;
+            btnStop.disabled = false;
+        }
+    }
+})();
--- tipcalculator/tip.js
+++ tipcalculator/tip.js
@@ -1,13 +1,12 @@
-
-function calcTip() {
-	var subtotalElem = document.getElementById('subtotal');
-	var tipElem = document.getElementById('tip');
-	var totalElem = document.getElementById('total');
-    var subtotal = parseFloat(subtotalElem.value !== "" ? subtotalElem.value : 0);
-    var tip = parseFloat(tipElem.value !== "" ? tipElem.value : 0);
-    
-
-
-	var total = subtotal + subtotal*tip/100;
-	totalElem.innerHTML = '$' + total;
-}
+const Calculator = (function(){
+	function calcTip() {
+		var subtotalElem = document.getElementById('subtotal');
+		var tipElem = document.getElementById('tip');
+		var totalElem = document.getElementById('total');
+		var subtotal = parseFloat(subtotalElem.value !== "" ? subtotalElem.value : 0);
+		var tip = parseFloat(tipElem.value !== "" ? tipElem.value : 0);
+		var total = subtotal + subtotal*tip/100;
+		totalElem.innerHTML = '$' + total;
+	}
+	return {do: function() {calcTip()}}
+})();
