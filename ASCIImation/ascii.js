53f666af7d16791f264183c2dbce2b0d2259482c
Han Truong <hantruongth@gmail.com>
2019-07-01 22:33:45 -0500

    update

--- ASCIImation/ascii.js
+++ ASCIImation/ascii.js
@@ -1,99 +1,86 @@
+"use strict";
 
-(function () {
-    "use strict";
+var timer = null;
+var animationArea;
+var animationArray = [];
+var start = 0;
+var end = 0;
+var interval = 250;
+var i;
+var animationType;
+var animationsList = ANIMATIONS; // from animations.js provided 
 
-    var timer = null;
-    var animationArea;
-    var animationArray = [];
-    var start = 0;
-    var end = 0;
-    var interval = 250;
-    var i;
-    var animationType;
-    var animationsList = ANIMATIONS;
+window.onload = function () {
+    var btnPlay = document.getElementById('btnPlay');
+    var btnStop = document.getElementById('btnStop');
+    var turboCheckbox = document.getElementById('turboCheckbox');
+    var animationSize = document.getElementById('animationSize');
+    animationArea = document.getElementById("animationArea");
+    animationType = document.getElementById('animationType');
 
-    window.onload = function () {
-        var btnPlay = document.getElementById('btnPlay');
-        var btnStop = document.getElementById('btnStop');
-        var turboCheckbox = document.getElementById('turboCheckbox');
-        var animationSize = document.getElementById('animationSize');
-        animationArea = document.getElementById("animationArea");
-        animationType = document.getElementById('animationType');
-
-        btnStop.display = 'none';
-
-        animationType.onchange = function () {
-            play();
-        };
-
-
-        btnPlay.onclick = function () {
-            play();
-        };
-
-        btnStop.onclick = function () {   
-            animationArea.innerHTML = "";
-            // btnPlay.display = 'block';
-            // btnStop.display = 'none';
-            clearInterval(timer);
-            timer = null; 
-            setBtnState();
-            
-        };
-
-        turboCheckbox.onchange = function () {
-            if (turboCheckbox.checked) {
-                interval = 50;
-            } else {
-                interval = interval * 5;
-            }
-            regenerate();
-        };
-
-
-        animationSize.onchange = function () {
-            animationArea.style.fontSize = this.options[this.selectedIndex].value + "pt";
-            regenerate();
-        };
+    animationType.onchange = function () {
+        play();
     };
 
-    function regenerate() {
+    btnPlay.onclick = function () {
+        play();
+    };
+
+    btnStop.onclick = function () {   
+        animationArea.innerHTML = "";
         clearInterval(timer);
-        timer = setInterval(animate, interval);
-    }
-
-    function setBtnState(){
-        if(timer == null){
-            btnStop.disabled = true;
-            btnPlay.disabled = false;
-        }else{
-            btnPlay.disabled = true;
-            btnStop.disabled = false;
-        }
-    }
-
-    function play() {
-        var selectedIndex = animationType.options[animationType.selectedIndex].value;
-        if (selectedIndex < 0) return;
-        animationArray = animationsList[selectedIndex].split("=====\n");
-        i = start;
-        end = animationArray.length;
-        if (timer === null) {
-            timer = setInterval(animate, interval);
-        }
+        timer = null; 
         setBtnState();
-    
-    }
+        
+    };
 
-    function animate() {
-        if (i < end) {
-            animationArea.value = animationArray[i];
-            i++;
+    turboCheckbox.onchange = function () {
+        if (turboCheckbox.checked) {
+            interval = 50;
+        } else {
+            interval = 250;
         }
-        else {
-            i = start;
-            animationArea.value = animationArray[i];
-        }
+        restart();
+    };
+    animationSize.onchange = function () {
+        animationArea.style.fontSize = this.options[this.selectedIndex].value + "pt";
+        restart();
+    };
+};
+function play() {
+    var selectedIndex = animationType.options[animationType.selectedIndex].value;
+    if (selectedIndex < 0) return;
+    animationArray = animationsList[selectedIndex].split("=====\n");
+    i = start;
+    end = animationArray.length;
+    if (timer === null) {
+        timer = setInterval(animate, interval); // interval 250
     }
+    setBtnState();
 
-})();
+}
+
+function animate() {
+    if (i < end) {
+        animationArea.value = animationArray[i];
+        i++;
+    }
+    else {
+        i = start;
+        animationArea.value = animationArray[i];
+    }
+}
+function restart() {
+    clearInterval(timer);
+    timer = setInterval(animate, interval);
+}
+
+function setBtnState(){
+    if(timer == null){
+        btnStop.disabled = true;
+        btnPlay.disabled = false;
+    }else{
+        btnPlay.disabled = true;
+        btnStop.disabled = false;
+    }
+}
