858c1dba310843362530f60ed3acfe611ca7bfaa
Han Truong <hantruongth@gmail.com>
2019-07-02 21:10:29 -0500

    update

---
+++ Lab5-JsExercise/buggy4.js
@@ -0,0 +1,12 @@
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    }
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+
+function gogogo() {
+    alert("Yay, it works!");
+}
